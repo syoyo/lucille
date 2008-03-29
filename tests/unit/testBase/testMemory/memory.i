24c87b7a3bbf277125ae65fedac329b4ab0c6eb2
syoyo <syoyo@c7d432cb-e41a-0410-8791-fcf357cca5d8>
2008-03-29 05:08:12 +0000

    Added tests for aligned_malloc().
    git-svn-id: https://lucille.svn.sourceforge.net/svnroot/lucille/trunk@164 c7d432cb-e41a-0410-8791-fcf357cca5d8

--- tests/unit/testBase/testMemory/memory.i
+++ tests/unit/testBase/testMemory/memory.i
@@ -4,3 +4,7 @@
 %}
 
 %include "../../../../src/base/memory.h"
+
+// private functions
+void *aligned_malloc(size_t size, unsigned int align);
+int   aligned_free(void *addr);
--- tests/unit/testBase/testMemory/test.py
+++ tests/unit/testBase/testMemory/test.py
@@ -1,6 +1,7 @@
 from base_memory import *
 
 import os, sys
+import random
 
 class TestMemoryAllocDoesNotReturnNull():
 
@@ -26,8 +27,35 @@ class TestMemoryFreeReturnsZero():
     def test(self):
         ret = ri_mem_free(self.mem)
 
-        assert ret == 0
+        assert (ret == 0), ret
 
+
+class TestAlignedMallocWithRandomArg():
+
+    def setup(self):
+        pass
+
+    def teardown(self):
+        pass
+
+    def test(self):
+
+        nTests = 10000
+
+        for i in range(nTests):
+        
+            sz    = random.randint(0, 1024*1024)
+            align = random.randint(0, 1024)
+
+            print "sz = %d, align = %d" % (sz, align)
+
+            p = aligned_malloc(sz, align);
+
+            assert int(p) % 16 == 0, "Not 16-byte aligned: addr(p) = %d" % int(p)
+
+            aligned_free(p)
+
+        
 
 
 # class TestMemoryAllocWithLargeSizeWillReturnNull():
