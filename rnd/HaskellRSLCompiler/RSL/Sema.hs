f0de99ccbfab72177f432f2db7ee8257de076936
syoyo <syoyo@x40.(none)>
2008-12-14 11:44:05 +0900

    Add more builtin definitions.

--- rnd/HaskellRSLCompiler/RSL/Sema.hs
+++ rnd/HaskellRSLCompiler/RSL/Sema.hs
@@ -39,15 +39,73 @@ builtinShaderVariables =
 builtinShaderFunctions :: [Symbol]
 builtinShaderFunctions =
   [ 
+  -- 15.1 Mathematical Functions
+    (SymFunc "radians"     f [f]    [])
+  , (SymFunc "degrees"     f [f]    [])
+  , (SymFunc "sin"         f [f]    [])
+  , (SymFunc "asin"        f [f]    [])
+  , (SymFunc "cos"         f [f]    [])
+  , (SymFunc "acos"        f [f]    [])
+  , (SymFunc "tan"         f [f]    [])
+  , (SymFunc "atan"        f [f]    [])
+  , (SymFunc "atan"        f [f, f] [])
+  , (SymFunc "pow"         f [f, f] [])
+  , (SymFunc "exp"         f [f]    [])
+  , (SymFunc "sqrt"        f [f]    [])
+  , (SymFunc "inversesqrt" f [f]    [])
+  , (SymFunc "log"         f [f]    [])
+  , (SymFunc "log"         f [f, f] [])
+  , (SymFunc "mod"         f [f, f] [])
+  , (SymFunc "abs"         f [f]    [])
+  , (SymFunc "sign"        f [f]    [])
+  , (SymFunc "min"         f [f, f] []) -- float, point, vector, normal, color
+  , (SymFunc "max"         f [f, f] []) -- float, point, vector, normal, color
+  , (SymFunc "clamp"       f [f, f] []) -- float, point, vector, normal, color
+  , (SymFunc "mix"         f [f, f, f] []) -- float, point, vector, normal, color
+  -- TODO...
+
   -- 15.2 Geometric Functions
-    (SymFunc "length"      f [v]    [])
-  , (SymFunc "normalize"   v [v]    [])
-  , (SymFunc "distance"    f [p, p] [])
-  , (SymFunc "faceforward" v [v, v] [])
+  , (SymFunc "xcomp"       f [v]       [])
+  , (SymFunc "xcomp"       f [p]       [])
+  , (SymFunc "xcomp"       f [n]       [])
+  , (SymFunc "ycomp"       f [v]       [])
+  , (SymFunc "ycomp"       f [p]       [])
+  , (SymFunc "ycomp"       f [n]       [])
+  , (SymFunc "zcomp"       f [v]       [])
+  , (SymFunc "zcomp"       f [p]       [])
+  , (SymFunc "zcomp"       f [n]       [])
+  -- , (SymFunc "setxcomp"       f [n]    [])
+  -- , (SymFunc "setycomp"       f [n]    [])
+  -- , (SymFunc "setzcomp"       f [n]    [])
+  , (SymFunc "length"      f [v]       [])
+  , (SymFunc "normalize"   v [v]       [])
+  , (SymFunc "distance"    f [p, p]    [])
+  -- , (SymFunc "ptlined"    f [p, p] [])
+  -- , (SymFunc "rotate"    f [p, p] [])
+  , (SymFunc "area"        f [p]       [])
+  , (SymFunc "faceforward" v [v, v]    []) -- has opt arg
+  , (SymFunc "reflect"     v [v, v]    [])
+  , (SymFunc "refract"     v [v, v, f] [])
+  , (SymFunc "fresnel"     v [v, v, f, f, f] [])  -- TODO
+  , (SymFunc "transform"   p [s, p]    []) 
+  , (SymFunc "transform"   p [s, s, p] []) 
+  , (SymFunc "transform"   p [m, p]    []) 
+  , (SymFunc "transform"   p [s, m, p] []) 
+  , (SymFunc "transform"   v [s, v]    []) 
+  , (SymFunc "transform"   v [s, s, v] []) 
+  , (SymFunc "transform"   v [m, v]    []) 
+  , (SymFunc "transform"   v [s, m, v] []) 
+  , (SymFunc "transform"   n [s, n]    []) 
+  , (SymFunc "transform"   n [s, s, n] []) 
+  , (SymFunc "transform"   n [m, n]    []) 
+  , (SymFunc "transform"   n [s, m, n] []) 
+  , (SymFunc "depth"       f [p]       []) 
+  , (SymFunc "depth"       f [p]       []) 
 
   -- 15.6 Shading and Lighting Functions
   , (SymFunc "ambient"     c []     [])
   , (SymFunc "diffuse"     c []     [])
+
 
   ] -- More is TODO
 
@@ -57,5 +115,9 @@ builtinShaderFunctions =
     c = TyColor
     v = TyVector
     p = TyPoint
+    n = TyNormal
+    s = TyString
+    m = TyMatrix
+    void = TyVoid
 
 
