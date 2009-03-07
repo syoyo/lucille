8679af5f4def05baf1144fbae0bc64475a116a52
syoyo <syoyo@lucillerender.org>
2009-03-07 23:22:42 +0900

    Insert copyright notice.

--- src/base/log.c
+++ src/base/log.c
@@ -1,8 +1,47 @@
 /*
- * Log system.
  *
- * $Id: log.c,v 1.5 2004/08/15 05:19:39 syoyo Exp $
+ *                   lucille | Global Illumination Renderer
+ *
+ *         Copyright 2003-2203 Syoyo Fujita (syoyo@lucillerender.org)
+ *
+ *
  */
+
+/*
+ * Copyright 2003-2203 Syoyo Fujita.  All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the names of the authors nor the names of their contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHORS ``AS IS'' AND ANY EXPRESS
+ * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHORS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+ * OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
+ * OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+ * BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+ * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+/* ---------------------------------------------------------------------------
+ *
+ * Simple logging system.
+ *
+ * ------------------------------------------------------------------------ */
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
